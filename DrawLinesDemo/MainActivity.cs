2a9d733ecbaa65b2c1524ae9547d18f6796caa41
Sebastian Seidel <buero@cayas.de>
2014-06-30 17:01:36 +0200

    Init import

---
+++ DrawLinesDemo/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Graphics;
+using Android.OS;
+
+namespace DrawLinesDemo
+{
+    [Activity(Label = "DrawLinesDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            var series = new List<StackedProductivityItem>();
+            series.Add(new StackedProductivityItem(74, Color.Rgb(179, 198, 24))); // green
+            series.Add(new StackedProductivityItem(5, Color.Rgb(129, 164, 188))); // blue
+            series.Add(new StackedProductivityItem(6, Color.Rgb(225, 112, 54))); // red
+            series.Add(new StackedProductivityItem(15, Color.Rgb(253, 213, 86))); // yellow
+
+            var stackedView = FindViewById<StackedProductivityView>(Resource.Id.StackedProductivityChart);
+            stackedView.Stretch = false;
+            stackedView.SetSeries(series);
+        }
+    }
+}
---
+++ DrawLinesDemo/StackedProductivityView.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+
+namespace DrawLinesDemo
+{
+    public class StackedProductivityView : View
+    {
+        List<StackedProductivityItem> series;
+        float startPoint;
+
+        public List<StackedProductivityItem> Series
+        {
+            get
+            {
+                return series;
+            }
+            set
+            {
+                series = value;
+                PostInvalidate();
+            }
+        }
+
+        public bool Stretch
+        {
+            get;
+            set;
+        }
+
+        public StackedProductivityView(Context context) : base(context)
+        {
+            Initialize();
+        }
+
+        public StackedProductivityView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public StackedProductivityView(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        public StackedProductivityView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            series = new List<StackedProductivityItem>();
+        }
+
+        public void SetSeries(List<StackedProductivityItem> series)
+        {
+            this.series = series;
+            Invalidate();
+            RequestLayout();
+        }
+
+        protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            SetMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+            base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            canvas.DrawColor(Color.Gray);
+
+            var itemLength = 1f;
+
+//            if (Stretch)
+//                itemLength = canvas.Width / series.Sum(p => p.ProductivityValue);
+
+            var y = 1;
+            var paint = new Paint();
+            paint.SetStyle(Paint.Style.Fill);
+
+            Console.WriteLine("ItemLength: " + itemLength);
+            Console.WriteLine("ItemLength Streched: " + Stretch);
+
+            foreach (var productivityItem in series)
+            {
+                var width = productivityItem.ProductivityValue;
+
+                if (width.CompareTo(0) == 0)
+                    continue;
+
+                paint.Color = productivityItem.Color;
+                paint.StrokeWidth = canvas.Height / series.Count;
+
+                var startx = startPoint;
+                var starty = y * paint.StrokeWidth;
+                var stopx = width * itemLength;
+                var stopy = y * paint.StrokeWidth;
+
+                Console.WriteLine("\r\nY-Axis: " + y);
+                Console.WriteLine("Width (ProductivityValue): " + width);
+                Console.WriteLine("PaintStrokeWidth: " + paint.StrokeWidth);
+
+                Console.WriteLine("StartPoint: " + startPoint);
+
+                Console.WriteLine("Start X: " + startx);
+                Console.WriteLine("Stop X: " + stopx);
+
+                Console.WriteLine("Start Y: " + starty);
+                Console.WriteLine("Stop Y: " + stopy);
+
+                canvas.DrawLine(startx, starty, stopx, stopy, paint);
+
+                startPoint += width * itemLength;
+
+                y++;
+            }
+        }
+    }
+
+    public class StackedProductivityItem
+    {
+        public Color Color
+        {
+            get;
+            set;
+        }
+
+        public float ProductivityValue
+        {
+            get;
+            set;
+        }
+
+        public StackedProductivityItem()
+        { }
+
+        public StackedProductivityItem(float productivityValue, Color color)
+        {
+            ProductivityValue = productivityValue;
+            Color = color;
+        }
+    }
+}
